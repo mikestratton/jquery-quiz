ea819edccf2ee07325f13cc6da34bb9faf5678d1
mikestratton <mike@mikestratton.net>
2017-04-12 19:30:48 -0400

    if score == range, return message

--- js/script.js
+++ js/script.js
@@ -26,6 +26,26 @@ function handleClick(){
           amountCorrect = amountCorrect + 5;
         }
       }
-    }                   
+    }         
+    
+    if(amountCorrect <= 30) {
       document.getElementById("total").innerHTML = "Your Score: " + amountCorrect;
+      document.getElementById("results").innerHTML = "<strong>30 points or less:</strong> there is still a lot you need to do in order to access procurement opportunities and become a supplier for public or private organizations. Start by doing your market research and getting to know who your potential clients are. Get familiar with their activities, and their procurement needs, rules and procedures.";
+    }
+    
+    if(amountCorrect >= 31 && amountCorrect <= 60) {
+      document.getElementById("total").innerHTML = "Your Score: " + amountCorrect;
+      document.getElementById("results").innerHTML = "<strong>31 to 60 points:</strong> you have started your market research to access the procurement market and become a supplier, however there is a lot of room for improvement. Identify which specific areas you need to work harder. You do not have to do it all alone. Identify who in your organization can help you prepare for this process.";
+    }
+    
+    if(amountCorrect >= 61 && amountCorrect <= 69) {
+      document.getElementById("total").innerHTML = "Your Score: " + amountCorrect;
+      document.getElementById("results").innerHTML = "<strong>61 to 69 points:</strong> you are on the right track, but you still need to make some improvements in your plan and strategy in order to be successful in the procurement market. Assess which particular points you need to improve and concentrate on those.";
+    }
+    
+    if(amountCorrect >= 70 && amountCorrect <= 75) {
+      document.getElementById("total").innerHTML = "Your Score: " + amountCorrect;
+      document.getElementById("results").innerHTML = "<strong>70 to 75 points:</strong> you may need to work harder in a couple of areas, but you are ready to access the procurement market and become a supplier for public and private organizations.";
+    }
+
   }
